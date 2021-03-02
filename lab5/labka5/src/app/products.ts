4111c0ec8567b1043c489551021dfd0d2fca38ea
909meters <55340955+909meters@users.noreply.github.com>
2021-03-03 03:38:58 +0600

    Add files via upload

---
+++ lab5/labka5/src/app/products.ts
@@ -0,0 +1,220 @@
+export const products = [
+  { category_id: 0,
+    category_name: "Make-up",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Maybelline Sky High Washable Mascara Makeup, Volumizing Mascara, Buildable, Lengthening Mascara, Defining, Curling, Multiplying, Washable Very Black, 0.2 fl. oz.',
+      price: '$22.98',
+      description: 'Sky high lash impact from every angle, full volume meets limitless length',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Maybelline-Volumizing-Buildable-Lengthening-Multiplying/dp/B08H4FSGDW/ref=sr_1_2?dchild=1&qid=1614717192&rnid=16225006011&s=beauty-intl-ship&sr=1-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71aPOVjcHpL._SL1500_.jpg',
+      likes: 23202
+    },
+    {
+      id: 1,
+      name: 'Neutrogena Makeup Remover Facial Cleansing Towelette Singles, Daily Face Wipes to Remove Dirt, Oil, Makeup & Waterproof Mascara, Gentle, Alcohol-Free, Individually Wrapped, 20 Ct',
+      price: '$5.73',
+      description: '20-count of individually wrapped pre-moistened Neutrogena Make up Remover Face Wipe Singles. Individually wrapped facial cleansing Towelettes are perfectly protected so you always have a fresh and effective wipe for complete cleansing and makeup removal',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Neutrogena-Cleansing-Towelette-Waterproof-Individually/dp/B07GKVBQDG/ref=sr_1_8?dchild=1&qid=1614717192&rnid=16225006011&s=beauty-intl-ship&sr=1-8',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/819kWnxrBlL._SL1500_.jpg',
+      likes: 4839
+    },
+    {
+      id: 2,
+      name: 'NYX PROFESSIONAL MAKEUP Control Freak Eyebrow Gel',
+      price: '$4.82',
+      description: 'Fill And Hold Eyebrow Gel: This Eyebrow Gel offers a new way to tame those arches fast; The clear formula works great with powders and pencils to fill in brows, seal the deal and hold them in place',
+      rating: 4.3,
+      link: 'https://www.amazon.com/NYX-PROFESSIONAL-MAKEUP-Control-Eyebrow/dp/B00INCDKDQ/ref=sr_1_11?dchild=1&qid=1614717192&rnid=16225006011&s=beauty-intl-ship&sr=1-11',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51YOM7SG7OL._SL1000_.jpg', 
+      likes: 3942
+    },
+    {
+      id: 3,
+      name: 'NYX PROFESSIONAL MAKEUP The Brow Glue Instant Brow Styler',
+      price: '$6.49',
+      description: 'Instant Brow Styler: This non sticky and transparent brow glue gives you extra strong, 16 hour extreme hold instantly, allowing you to style brow hairs and hold them in place for limitless looks',
+      rating: 4.4,
+      link: 'https://www.amazon.com/NYX-PROFESSIONAL-MAKEUP-Instant-Styler/dp/B08M58SWB1/ref=sr_1_19?dchild=1&qid=1614717192&rnid=16225006011&s=beauty-intl-ship&sr=1-19',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71dXKKZevrL._SL1500_.jpg',
+      likes: 4939
+    },
+    {
+      id: 4,
+      name: 'Maybelline Instant Age Rewind Eraser Dark Circles Treatment Multi-Use Concealer, Fair, 0.2 Fl Oz (Pack of 1)',
+      price: '$7.45',
+      description: 'MAYBELLINE INSTANT AGE REWIND CONCEALER: Americas #1 Concealer! Erase the look of dark circles, correct the appearance of redness and brighten the look of dull skin with Instant Age Rewind Eraser multi-use concealer.',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Maybelline-Makeup-Instant-Rewind-Concealer/dp/B004Y9GTOO/ref=sr_1_20?dchild=1&qid=1614717192&rnid=16225006011&s=beauty-intl-ship&sr=1-20',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51lb-DT2I%2BL._SL1300_.jpg',
+      likes: 30449
+    }]
+  },
+  { category_id: 1,
+    category_name: "Skin Care",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'TruSkin Vitamin C Serum for Face with Hyaluronic Acid, Vitamin E, Witch Hazel, 1 fl oz',
+      price: '$19.99',
+      description: 'ANTIOXIDANT FACE SERUM - Vitamin C blends with Botanical Hyaluronic Acid, Vitamin E, Witch Hazel, and Jojoba Oil to promote your skins response to signs of aging like brightness, wrinkles, & dark spots',
+      rating: 4.3,
+      link: 'https://www.amazon.com/TruSkin-Naturals-Vitamin-Topical-Hyaluronic/dp/B01M4MCUAF/ref=sr_1_5?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614718379&rnid=16225006011&s=beauty-intl-ship&sr=1-5',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/516yN4iPdQL._SL1250_.jpg', 
+      likes: 3944
+    },
+    {
+      id: 1,
+      name: 'Aveeno Daily Moisturizing Body Lotion with Soothing Oat and Rich Emollients to Nourish Dry Skin, Gentle & Fragrance-Free Lotion is Non-Greasy & Non-Comedogenic, 18 Fl Oz',
+      price: '$8.54',
+      description: '18-fluid ounce pump bottle of Aveeno Daily Moisturizing Body Lotion to moisturize dry skin for a full 24 hours',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Aveeno-Moisturizing-Soothing-Emollients-Fragrance-Free/dp/B001459IEE/ref=sr_1_11?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614718473&rnid=16225006011&s=beauty-intl-ship&sr=1-11',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71fpYAPvK%2BL._SL1500_.jpg',
+      likes: 1002
+    },
+    {
+      id: 2,
+      name: 'Burts Bees 100% Natural Moisturizing Lip Balm, Original Beeswax with Vitamin E & Peppermint Oil – 4 Tubes',
+      price: '$9.57',
+      description: 'LIP CARE: Bursting with minty freshness, refresh and nourish your lips with Burts Bees Beeswax Moisturizing Lip Balm.',
+      rating: 4.8,
+      link: 'https://www.amazon.com/Burts-Bees-Moisturizing-Original-Peppermint/dp/B0054LHI5A/ref=sr_1_15?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614718473&rnid=16225006011&s=beauty-intl-ship&sr=1-15',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71V9BvmfSgL._SL1500_.jpg',
+      likes: 2030
+    },
+    {
+      id: 3,
+      name: 'Tree Hut Shea Sugar Scrub Tropical Mango, 18oz, Ultra Hydrating and Exfoliating Scrub for Nourishing Essential Body Care',
+      price: 	'$6.48',
+      description: '100% PURE NATURAL SHEA BUTTER - Premium deep moisturizer that wonderfully softens and smooths dry cracked skin.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Tree-Hut-Hydrating-Exfoliating-Nourishing/dp/B007DGRT3K/ref=sr_1_23?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614718473&rnid=16225006011&s=beauty-intl-ship&sr=1-23',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61OzSj4jB3L._SL1000_.jpg',
+      likes:5666
+    },
+    {
+      id: 4,
+      name: 'CeraVe Moisturizing Cream | Body and Face Moisturizer for Dry Skin | Body Cream with Hyaluronic Acid and Ceramides | 19 Ounce',
+      price: 86.00,
+      description: '[ HYALURONIC ACID MOISTURIZER ] With hyaluronic acid, ceramides and MVE technology for 24 hour hydration. Rich, velvety texture that leaves skin feeling smooth, it is absorbed quickly for softened skin without greasy, sticky, feel.',
+      rating: 4.8,
+      link: 'https://www.amazon.com/CeraVe-Moisturizing-Cream-Daily-Moisturizer/dp/B00TTD9BRC/ref=sr_1_3?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614718473&rnid=16225006011&s=beauty-intl-ship&sr=1-3',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61S7BrCBj7L._SL1000_.jpg',
+      likes: 4652
+    }]
+  },
+  { category_id: 2,
+    category_name: "Hair care",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'REVLON One-Step Hair Dryer And Volumizer Hot Air Brush, Black, Packaging May Vary',
+      price: '$51.63',
+      description: 'Delivers gorgeous volume and brilliant shine in a single step',
+      rating: 4.6,
+      link: 'https://www.amazon.com/Revlon-One-Step-Dryer-Volumizer-Brush/dp/B01LSUQSB0/ref=sr_1_2?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614718752&rnid=16225006011&s=beauty-intl-ship&sr=1-2',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71aXzv34N%2BL._SL1500_.jpg',
+      likes: 5022
+    },
+    {
+      id: 1,
+      name: 'Castor Oil (2oz) USDA Certified Organic, 100% Pure, Cold Pressed, Hexane Free by Kate Blanc. Stimulate Growth for Eyelashes, Eyebrows, Hair. Lash Growth Serum. Brow Treatment. FREE Mascara Starter Kit',
+      price: '$9.99',
+      description: 'Super Comprehensive Exercise Set- 11 Pcs Ab Wheel Roller Kit : 1* AB Roller Wheel, 1* Thick Foam Knee Mat , 2* Resistance bands, 2* Push Up Bars Handles Grips, 2* Wrist Wraps, 1* 8-shaped Stretched Band, 1* Jump Rope, 1* Guide Book, 1* Gift Box Package. 11-IN-1 ultra-wide ab roller wheel is designed to help you build stronger, bigger abdominal muscles. Improve your overall health and exercise at home, gym or office.',
+      rating: 4.5,
+      link: 'https://www.amazon.com/Certified-Kate-Stimulate-Eyelashes-Treatment/dp/B01NALN8Q9/ref=sr_1_6?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614719583&rnid=16225006011&s=beauty-intl-ship&sr=1-6',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/615AiiiHAUL._SL1100_.jpg',
+      likes: 3939
+    },
+    {
+      id: 2,
+      name: 'Hair Scalp Massager Shampoo Brush, MAXSOFT Scalp Care Brush',
+      price: '$8.49',
+      description: 'Shampoo brush with perfect grip handle to fit your hand comfortably (Manual with no battery required & Waterproof)',
+      rating: 4.6,
+      link: 'https://www.amazon.com/Scalp-Massager-Shampoo-Brush-MAXSOFT/dp/B074ZDXFL6/ref=sr_1_10?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614719583&rnid=16225006011&s=beauty-intl-ship&sr=1-10',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51EbgIgofzL._SL1036_.jpg',
+      likes: 7229
+    },
+    {
+      id: 3,
+      name: 'Equinox Professional Razor Edge Series Barber Hair Cutting Scissors - Japanese Stainless Steel Salon Scissors - 6.5” Overall Length - Fine Adjustment Tension Screw - Premium Shears for Hair Cutting',
+      price: '$25.97',
+      description: '[Patented Design] Our raving reviews speak for themselves. Customers LOVE our haircutting shears. PREMIUM - This specialized high quality Japanese stainless steel Hair Cutting scissor is tempered with precise blades and hand-sharpened cutting edges to evenly trim hair with ease.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Equinox-Professional-Shears-Razor-Edge/dp/B00KVYBY8M/ref=sr_1_19?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614719583&rnid=16225006011&s=beauty-intl-ship&sr=1-19',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61sKdG3rSZL._SL1500_.jpg',
+      likes: 2030
+    },
+    {
+      id: 4,
+      name: 'Fanola No Yellow Shampoo Large Bottle, 33.8 Fl Oz',
+      price: '$15.21',
+      description: 'Softens and smooths hair',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Fanola-Yellow-Shampoo-Large-Bottle/dp/B00CGPMEAQ/ref=sr_1_22?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614719583&rnid=16225006011&s=beauty-intl-ship&sr=1-22',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/517e7K-6kLL._SL1276_.jpg',
+      likes: 698
+    }]
+  },
+  { category_id: 3,
+    category_name: "Fragrance",
+    display_flag: false, 
+    items: [{
+      id: 0,
+      name: 'Cremo Bourbon & Oak Cologne Spray, A Sophisticated Blend of Distiller’s Spice, Fine Bourbon and White Oak, 3.4 Oz',
+      price: '$19.54',
+      description: 'A sophisticated blend of distiller’s spice, fine bourbon and white oak',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Cremo-Cologne-Spray-Bourbon-Ounce/dp/B076BYJL9T/ref=sr_1_1?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614719968&rnid=16225006011&s=beauty-intl-ship&sr=1-1',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/51kkDDYoEbL._SL1095_.jpg',
+      likes: 23811
+    },
+    {
+      id: 1,
+      name: 'Cuba Classic For Men Gift Set (4 x 1.17 Ounce EDT Spray)',
+      price: '	$13.13',
+      description: '4 Pc Gift Set Contains:- 1.17 ounce cuba gold, 1.17 ounce cuba blue, 1.17 ounce cuba red, 1.17 ounce cuba orange',
+      rating: 4.2,
+      link: 'https://www.amazon.com/Cuba-Classic-Gift-Ounce-Spray/dp/B0017LYVGU/ref=sr_1_5?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614720034&rnid=16225006011&s=beauty-intl-ship&sr=1-5',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81lfnijSw1L._SL1500_.jpg',
+      likes: 4493
+    },
+    {
+      id: 2,
+      name: 'Versace BRIGHT CRYSTAL ABSOLU 0.17 oz / 5 ml EDP SPLASH WOMEN NEW IN BOX MINI',
+      price: '$11.99',
+      description: 'Versace Bright Crystal Absolu By Gianni Versace',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Versace-BRIGHT-CRYSTAL-ABSOLU-SPLASH/dp/B00V5GSAYQ/ref=sr_1_24?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614720034&rnid=16225006011&s=beauty-intl-ship&sr=1-24',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/61m2SpJFfRL._SL1000_.jpg',
+      likes: 1035
+    },
+    {
+      id: 3,
+      name: 'Auric Blends Egyptian Goddess Roll-On Perfume 1/3 oz',
+      price: '$9.95',
+      description: 'Hand-blended with exotic oil ingredients from around the world',
+      rating: 4.4,
+      link: 'https://www.amazon.com/Auric-Blends-Egyptian-Goddess-Perfume/dp/B0000ZLEJG/ref=sr_1_15?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614720034&rnid=16225006011&s=beauty-intl-ship&sr=1-15',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71TAcml1a8L._SL1500_.jpg',
+      likes: 3347
+    },
+    {
+      id: 4,
+      name: 'Murray & Lanman Florida Water, 16 Fluid Ounce',
+      price: '$12.24',
+      description: 'Not tested on animals.',
+      rating: 4.7,
+      link: 'https://www.amazon.com/Murray-Lanman-Florida-Water-Fluid/dp/B0055DT4YU/ref=sr_1_3?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=SV8FNEXET0PB22QXJ7ZE&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1614720034&rnid=16225006011&s=beauty-intl-ship&sr=1-3',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81MNVayrzZL._AC_SL1500_.jpg',
+      likes: 3220
+    }]
+  }
+];
+
+
